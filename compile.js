e4081d21b2548654a0336eb0467fb1e6c374aac0
zen <mkhb654@gmail.com>
2018-02-03 13:49:21 -0500

    rewrite of test and deploy to ganache network

--- compile.js
+++ compile.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
-//_dirname is node
-const inboxPath = path.resolve(_dirname, 'contracts', 'Inbox.sol');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':inbox'];
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
